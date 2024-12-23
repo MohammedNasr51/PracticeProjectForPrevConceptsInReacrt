ce2c54671f2eecb2040181e1a3fef8e7d66d1cdd
Mohamed Nasr <mnasr0868@gmail.com>
2024-12-24 00:33:24 +0200

    adding inputs to the form of creating project and style it and seperate the component structure

---
+++ src/components/Input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Input({ label, isTextarea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+  return (
+    <p className="flex flex-col my-4 gap-1">
+      <label className="text-sm uppercase text-stone-500 font-bold">
+        {label}
+      </label>
+      {isTextarea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
+    </p>
+  );
+}
